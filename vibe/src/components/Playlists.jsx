eb400eac879270f684b851fabc93bfd93a0e4cd0
TirthPatel-tp <127706619+TirthPatel-tp@users.noreply.github.com>
2023-12-03 21:28:52 -0500

    Api - Vibe

---
+++ vibe/src/components/Playlists.jsx
@@ -0,0 +1,24 @@
+import React,{useEffect} from 'react'
+import { useStateProvider } from '../utils/StateProvider'
+import axios from 'axios';
+
+function Playlists() {
+   const[{token,dispatch}]= useStateProvider(); //Bringing token from the provider
+    useEffect(()=>{
+        const getPlaylistData = async ()=>{
+            const response = await axios.get('https://api.spotify.com/v1/me/playlists',{
+                headers:{
+                    "Authorization": "Bearer "+ token,
+                    "Content-Type":"application/json"
+                },
+            });
+            console.log(response);
+        }
+        getPlaylistData();
+    },[token,dispatch])
+  return (
+    <div>Playlists</div>
+  )
+}
+
+export default Playlists
--- vibe/src/components/Sidebar.jsx
+++ vibe/src/components/Sidebar.jsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import logo from '../img/logo_white.png'
 import { IoLibrary } from 'react-icons/io5'
 import { MdHomeFilled, MdSearch } from 'react-icons/md'
+import Playlists from './Playlists';
 
 function Sidebar() {
     return (
@@ -32,18 +33,48 @@ function Sidebar() {
                     </li>
                 </ul>
             </div>
+            <Playlists/>
         </Container>
     )
 }
 
 const Container = styled.div`
 background-color: black;
-color:#b3b3b3;
+color: #b3b3b3;
 display: flex;
 flex-direction: column;
 height:100%;
 width:100%;
-
+.top_links{
+    display:flex;
+    flex-direction: column;
+}
+.logo{
+    text-align: center;
+    margin: 1rem 0;
+    img{
+        max-inline-size:100%
+        block-size:auto;
+        margin-top: -4rem;
+    }
+}
+ul{
+    margin-top: -3rem;
+    list-style-type:none;
+    display:flex;
+    flex-direction:column;
+    gap: 1rem;
+    padding: 1 rem;
+    li{
+        display: flex;
+        gap:1 rem ;
+        cursor: pointer;
+        transition:0.3s ease-in-out;
+        &:hover{
+            color: white;
+        }
+    }
+}
 `
 
 export default Sidebar
